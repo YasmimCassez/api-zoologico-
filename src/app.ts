aae347c7a4d9e34441fc464c6c79a999cb420715
Yasmim Cassez <cassezyasmim@gmail.com>
2024-01-31 13:34:23 -0300

    Versionando a API

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const port = 3000;
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.json('Olá mundo!!');
+})
+
+server.listen(port, () => {
+    console.log('Servidor está escutando no endereço http://localhost:${port}');
+})
